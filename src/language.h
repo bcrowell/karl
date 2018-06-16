9240233f99490ee04e03d1d2347a6fe8ef1d5590
Ben Crowell <notreal@fakewillnotwork.invalid>
2018-06-16 11:59:00 -0700

    fix bug in IS_REAL macro; passes all tests

--- src/language.h
+++ src/language.h
@@ -1,6 +1,6 @@
 #if "LANG" eq "python"
 #define IS_NAN(x) math.isnan(x)
-#define IS_NONE(x) ((x) is not None)
+#define IS_NONE(x) ((x) is None)
 #define IS_REAL(x) (not (IS_NONE(x) or IS_NAN(x)))
 #define NAN float('nan')
   # ... note that NAN==NAN is false
