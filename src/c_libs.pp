5df925963cb79773bca0c10288b41066fac7a771
Ben Crowell <notreal@fakewillnotwork.invalid>
2018-06-25 10:47:44 -0700

    fix hardcoding of directory for karl.so

--- src/c_libs.pp
+++ src/c_libs.pp
@@ -1,10 +1,15 @@
 # Stuff for calling C subroutines.
 
 #if "LANG" eq "python"
-import ctypes
-karl_c_lib = ctypes.cdll.LoadLibrary('/home/bcrowell/Documents/programming/karl/obj/karl.so')
-# ... fixme: don't hardcode the directory
+
+import ctypes,os,inspect
+
+mydir = os.path.dirname(os.path.abspath(inspect.getframeinfo(inspect.currentframe()).filename))
+# ... the directory in which the currently running script is -- https://stackoverflow.com/a/6209894/1142217
+karl_c_lib = ctypes.cdll.LoadLibrary(mydir+'/karl.so')
+
 c_double_p = ctypes.POINTER(ctypes.c_double)
+
 #endif
 
 
